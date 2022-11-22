5ad3291207d332788a21672e2916864d4884ffe9
Alok3333 <alokprajapati697@gmail.com>
2022-11-22 23:04:51 +0530

    Create a login form & Handle value on this form

--- src/App.js
+++ src/App.js
@@ -2,30 +2,29 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Hedaer.jsx';
 import TopNotification from './Components/Notifications/Top_Notification';
+// import UseReducer from './Components/useReducer/useReduce';
 // import LinksExample from './Components/link';
 // import  CardGroup  from '../src/Components/Cards/card';
 import Login from '../src/pages/login/Login';
 // import UserList from './pages/userList/UserList';
-import Todolist from './pages/TodoList/Todolist';
+// import Todolist from './pages/TodoList/Todolist';
 
 function App() {
   return (
     <>
       <div className="main">
-        <Header/>
-        <TopNotification/>
+        <Header />
+        <TopNotification />
         <div className="container">
+          <Login />
           {/* <UserList/> */}
-          <Todolist/>
+          {/* <Todolist/> */}
+          {/* <UseReducer/> */}
         </div>
-        
-
       </div>
-        {/* <LinksExample/> */}
-        {/* <CardGroup/> */}
-        <Login/>
-        <button className='btn btn-danger'>what is memo in react ?</button>
-      
+      {/* <LinksExample/> */}
+      {/* <CardGroup/> */}
+      <button className="btn btn-danger">what is memo in react ?</button>
     </>
   );
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,50 @@
+import { useState } from 'react';
+import './login.css';
+
 const Login = () => {
-    return (
-        <div>
-            <h2> Login Form !!! </h2>
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const userNameHandler = (event) => {
+        // console.log(event.target.value);
+        setUsername(event.target.value)
+    }
+
+    const passwordNameHandler = (event) => {
+        // console.log(event.target.value);
+        setPassword(event.target.value)
+    }
+    
+  return (
+    <div className="form-box">
+      <form>
+        <h2>Login Form</h2>
+        <div className="form-group">
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={userNameHandler}
+            placeholder="Username"
+            autoComplete="false"
+          />
         </div>
-    )
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={passwordNameHandler}
+            placeholder="Password"
+            autoComplete="false"
+          />
+        </div>
+        <div className="submitBtn">
+          <button type="submit">Sign In</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
